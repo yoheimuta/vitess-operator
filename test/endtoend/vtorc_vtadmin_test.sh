8a1bc5f1dd93d049291f3663e0f47daeeb738c39
Yohei Yoshimuta <yoheimuta@gmail.com>
2024-07-11 16:54:15 +0900

    reduce cpu resource requests
    Signed-off-by: Yohei Yoshimuta <yoheimuta@gmail.com>

--- test/endtoend/vtorc_vtadmin_test.sh
+++ test/endtoend/vtorc_vtadmin_test.sh
@@ -14,7 +14,7 @@ function get_started_vtorc_vtadmin() {
     checkPodStatusWithTimeout "example-zone1-vtctld(.*)1/1(.*)Running(.*)"
     checkPodStatusWithTimeout "example-zone1-vtgate(.*)1/1(.*)Running(.*)"
     checkPodStatusWithTimeout "example-etcd(.*)1/1(.*)Running(.*)" 3
-    checkPodStatusWithTimeout "example-vttablet-zone1(.*)3/3(.*)Running(.*)" 3
+    checkPodStatusWithTimeout "example-vttablet-zone1(.*)3/3(.*)Running(.*)" 2
     checkPodStatusWithTimeout "example-zone1-vtadmin(.*)2/2(.*)Running(.*)"
     checkPodStatusWithTimeout "example-commerce-x-x-zone1-vtorc(.*)1/1(.*)Running(.*)"
 
